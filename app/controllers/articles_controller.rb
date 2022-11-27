cd2ba42236ac52a03815f547d35501fa8fb8c2a5
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-28 01:39:57 +0300

    completed lab

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,17 @@ class ArticlesController < ApplicationController
     render json: articles, each_serializer: ArticleListSerializer
   end
 
+  #If the user has viewed fewer than 3 pages, render a JSON response with the article data.
+
   def show
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    unless session[:page_views] > 3
     article = Article.find(params[:id])
     render json: article
+    else
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+    end
   end
 
   private
